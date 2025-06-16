9938cfedb4f06de670c3389067f027b766cef594
bubai14 <bubaibal14@gmail.com>
2025-06-16 15:23:57 +0530

    Initial commit

--- src/main/java/org/aws/springaiamazonbedrock/controller/ChatController.java
+++ src/main/java/org/aws/springaiamazonbedrock/controller/ChatController.java
@@ -20,18 +20,36 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    /**
+     * Endpoint to handle chat requests
+     *
+     * @param request The chat request containing the prompt
+     * @return A response entity with the chat response
+     */
     @PostMapping("/api/chat")
     public ResponseEntity<ChatResponse> chat(@RequestBody ChatRequest request) {
         String response = chatService.chat(request.getPrompt());
         return ResponseEntity.ok(new ChatResponse(response));
     }
 
+    /**
+     * Endpoint to handle chat requests with a path variable
+     *
+     * @param city The city to be used in the chat
+     * @return A response entity with the chat response
+     */
     @PostMapping("/api/temperature/{city}")
     public ResponseEntity<ChatResponse> chatWithTemplate(@PathVariable String city) {
         String response = chatService.chatWithTemplate(city);
         return ResponseEntity.ok(new ChatResponse(response));
     }
 
+    /**
+     * Endpoint to handle chat requests with a request body
+     *
+     * @param cityTemperatureRequest The request body containing the city
+     * @return A response entity with the chat response
+     */
     @PostMapping("/api/temperature/format")
     public ResponseEntity<CityTemperatureResponse> chatWithFormat(@RequestBody CityTemperatureRequest cityTemperatureRequest) {;
         return ResponseEntity.ok(chatService.chatWithFormat(cityTemperatureRequest));
