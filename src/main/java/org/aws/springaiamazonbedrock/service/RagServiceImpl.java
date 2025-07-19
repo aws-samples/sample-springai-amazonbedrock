ddb057248f5081db65ba2a4137b76e9c23d2ba91
bubai14 <bubaibal14@gmail.com>
2025-07-19 10:58:24 +0530

    updated the skeleton of the service classes

--- src/main/java/org/aws/springaiamazonbedrock/service/RagServiceImpl.java
+++ src/main/java/org/aws/springaiamazonbedrock/service/RagServiceImpl.java
@@ -4,6 +4,7 @@ package org.aws.springaiamazonbedrock.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.ai.embedding.EmbeddingResponse;
 
 import java.util.List;
 import java.util.Map;
@@ -12,4 +13,13 @@ import java.util.Map;
 public class RagServiceImpl implements RagService {
 
 
+    @Override
+    public Map<String, EmbeddingResponse> embed(String messages) {
+        return Map.of();
+    }
+
+    @Override
+    public String getResponse(String question) {
+        return "";
+    }
 }
--- src/main/java/org/aws/springaiamazonbedrock/service/SimpleChatServiceImpl.java
+++ src/main/java/org/aws/springaiamazonbedrock/service/SimpleChatServiceImpl.java
@@ -1,6 +1,8 @@
 package org.aws.springaiamazonbedrock.service;
 
 
+import org.aws.springaiamazonbedrock.model.CityTemperatureRequest;
+import org.aws.springaiamazonbedrock.model.CityTemperatureResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -11,4 +13,18 @@ import java.util.Map;
 @Service
 public class SimpleChatServiceImpl implements SimpleChatService {
 
+    @Override
+    public String chat(String prompt) {
+        return "";
+    }
+
+    @Override
+    public String chatWithTemplate(String city) {
+        return "";
+    }
+
+    @Override
+    public CityTemperatureResponse chatWithFormat(CityTemperatureRequest cityTemperatureRequest) {
+        return null;
+    }
 }
